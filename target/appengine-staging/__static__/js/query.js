dcdc2a88ae996f222e6e67466ba52a1efd3044a0
ryan <ryangudal@gmail.com>
2020-06-30 20:30:30 +0000

    Set Up Server

--- target/appengine-staging/__static__/js/query.js
+++ target/appengine-staging/__static__/js/query.js
@@ -62,3 +62,11 @@ function query() {
         .then(() => console.log(searchResults))
         .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"));
 }
+
+function onSignIn(googleUser) {
+  let profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
