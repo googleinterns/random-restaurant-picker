7cc9f5b8b37c162874f7473211eed18b471de691
Emma Dunn <evd1@rice.edu>
2020-07-07 20:17:21 +0000

    javascript updates

--- src/main/java/com/google/sps/servlets/accountServlet.java
+++ src/main/java/com/google/sps/servlets/accountServlet.java
@@ -16,24 +16,44 @@ package com.google.sps.servlets;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+
+import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet responsible for deleting tasks. */
-@WebServlet("/delete-comment")
-public class DeleteCommentServlet extends HttpServlet {
+
+@WebServlet("/account-page")
+public class AccountServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    
+    
+    response.setContentType("application/json");
+    Gson gson = new Gson();
+    gson.toJson(gson.toJsonTree(voteCount), gson.newJsonWriter(response.getWriter()));
+  }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    long id = Long.parseLong(request.getParameter("id"));
+    String continent = request.getParameter("continent");
 
-    Key commentEntityKey = KeyFactory.createKey("Comment", id);
+    // creates Entity for continent vote
+    Entity continentEntity = new Entity("Continent");
+    continentEntity.setProperty("continent", continent);
+
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    datastore.delete(commentEntityKey);
+        datastore.put(continentEntity);
+
+    response.sendRedirect("/account-info.html");
   }
 }
