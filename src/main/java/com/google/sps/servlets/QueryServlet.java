5e5b29cfd82f2bfc3665acf8142d16e7d6e4168d
ryan <ryangudal@gmail.com>
2020-07-10 20:37:34 +0000

    Create a class that accesses secretmanager

--- src/main/java/com/google/sps/servlets/QueryServlet.java
+++ src/main/java/com/google/sps/servlets/QueryServlet.java
@@ -27,6 +27,7 @@ import java.io.IOException;
 import com.google.sps.data.Response;
 import com.google.sps.data.Restaurant;
 import com.google.sps.data.User;
+import com.google.sps.data.AccessSecret;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -43,8 +44,9 @@ import java.util.ArrayList;
 @WebServlet("/query")
 public class QueryServlet extends HttpServlet {
 
-    private final String apiKey = "AIzaSyBL_9GfCUu7DGDvHdtlM8CaAywE2bVFVJc";
-    private final Gson gson = new GsonBuilder().create();
+    private final String apiKey = AccessSecret.accessSecretVersion();
+    private final Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+    private Response response;
     private User user;
 
     @Override
