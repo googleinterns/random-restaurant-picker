b2d6c7c6d8eca5e90fff99c6d3627de8183348a4
ryan <ryangudal@gmail.com>
2020-07-16 22:30:54 +0000

    Tests for QueryServlet

--- src/main/java/com/google/sps/servlets/QueryServlet.java
+++ src/main/java/com/google/sps/servlets/QueryServlet.java
@@ -25,6 +25,7 @@ import java.io.IOException;
 import com.google.sps.data.Response;
 import com.google.sps.data.Restaurant;
 import com.google.sps.data.User;
+import com.google.sps.data.UrlOpener;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -44,7 +45,16 @@ public class QueryServlet extends HttpServlet {
     private final String apiKey = "AIzaSyBL_9GfCUu7DGDvHdtlM8CaAywE2bVFVJc";
     private final Gson gson = new GsonBuilder().create();
     private User user;
+    private UrlOpener urlOpener;
 
+    public QueryServlet(UrlOpener opener){
+        this.urlOpener = opener;
+    }
+
+    public QueryServlet(){
+        this.urlOpener = new UrlOpener();
+    }
+    
     @Override
     public void doGet(HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws IOException {
         HttpSession session = servletRequest.getSession(false);
@@ -65,10 +75,7 @@ public class QueryServlet extends HttpServlet {
         String searchTerms = servletRequest.getParameter("searchTerms");
         String urlStr = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + lon + "&radius=" + radius + "&type=" + type + "&keyword=" + searchTerms + "&key=" + apiKey;
 
-        URLConnection conn = new URL(urlStr).openConnection();
-        conn.connect();
-
-        JsonElement jsonElement = new JsonParser().parse(new InputStreamReader(conn.getInputStream()));
+        JsonElement jsonElement = urlOpener.openUrl(urlStr);
         JsonObject responseJson = jsonElement.getAsJsonObject();
         Response response = gson.fromJson(responseJson, Response.class);
 
