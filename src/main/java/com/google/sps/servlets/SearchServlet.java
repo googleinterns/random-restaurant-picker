3be9d57ccb51f438b3a20e9802011f6de8534deb
ryan <ryangudal@gmail.com>
2020-07-16 21:12:05 -0400

    Beginning of SearchServletTest

--- src/main/java/com/google/sps/servlets/SearchServlet.java
+++ src/main/java/com/google/sps/servlets/SearchServlet.java
@@ -40,13 +40,14 @@ import com.google.sps.data.SearchItem;
 @WebServlet("/searches")
 public class SearchServlet extends HttpServlet {
 
+    private DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String user = request.getParameter("user");
         Filter propertyFilter = new FilterPredicate("user", FilterOperator.EQUAL, user);
         Query query = new Query("savedSearch").setFilter(propertyFilter).addSort("timestamp", SortDirection.DESCENDING);
 
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         PreparedQuery results = datastore.prepare(query);
 
         List <SearchItem> searches = new ArrayList<>();
@@ -87,8 +88,6 @@ public class SearchServlet extends HttpServlet {
         searchEntity.setProperty("timestamp", timestamp);
         searchEntity.setProperty("lat", lat);
         searchEntity.setProperty("lng", lng);
-
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         datastore.put(searchEntity);
 
         // Redirect back to the HTML page.
