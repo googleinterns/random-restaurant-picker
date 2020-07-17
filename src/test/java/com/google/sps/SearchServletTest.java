11124ddf7f2cf56f3ea720d5cd9d1aa2d372d664
ryan <ryangudal@gmail.com>
2020-07-16 21:12:05 -0400

    Beginning of SearchServletTest

---
+++ src/test/java/com/google/sps/SearchServletTest.java
@@ -0,0 +1,100 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps;
+
+import com.google.sps.servlets.ConvertLocationServlet;
+import com.google.sps.data.UrlOpener;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.Mock;
+import org.mockito.ArgumentCaptor;
+import static org.mockito.ArgumentMatchers.*;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+
+@RunWith(MockitoJUnitRunner.class)
+public final class QueryServletTest {
+  private final LocalServiceTestHelper helper = 
+    new LocalServiceTestHelper(
+        new LocalDatastoreServiceTestConfig()
+            .setDefaultHighRepJobPolicyUnappliedJobPercentage(0));
+  
+  @Mock
+  HttpServletRequest request;
+  @Mock
+  HttpServletResponse response;
+  @Mock
+  HttpSession session;
+  
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
+    helper.setUp();
+  }
+
+  @After
+  public void done() throws Exception {
+      helper.tearDown();
+  }
+
+  @Test
+  public void POSTTest() throws IOException{
+      when(request.getParameter("lat")).thenReturn("40");
+      when(request.getParameter("lng")).thenReturn("-80");
+      when(request.getParameter("user")).thenReturn("1");
+      when(request.getParameter("radisu")).thenReturn("1000");
+      when(request.getParameter("keywords")).thenReturn("coffee");
+
+      new SearchServlet().doPost(request, response);
+      verify(response).sendRedirect("/index.html");
+
+      DatastoreService ds = DatastoreServiceFactory.getDataStoreService();
+      assertEquals(1, ds.prepare(new Query("savedSearch")).countEntities());
+
+  }
+
+}
