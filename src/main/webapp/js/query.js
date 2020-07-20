cebea4f70d191ab43c813e1e2b289aec5ec49815
ryan <ryangudal@gmail.com>
2020-07-20 15:47:20 +0000

    Add Map to the resultsPage

--- src/main/webapp/js/query.js
+++ src/main/webapp/js/query.js
@@ -42,6 +42,7 @@ function query(queryStr) {
                 let rating = response.pick.rating + ' ★';
                 let photoUrl = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=600&photoreference=' + response.pick.photos[0].photoReference + '&key=AIzaSyBL_9GfCUu7DGDvHdtlM8CaAywE2bVFVJc';
                 errorEl.innerText = name;
+                localStorage.setItem("restaurantAddress", response.pick.vicinity);
                 resultsPage(name, rating, photoUrl);
             } else if (response.status === "INVALID_REQUEST") throw "Invalid request";
             else if (response.status === "ZERO_RESULTS") throw "No results";
@@ -66,7 +67,9 @@ function reroll() {
                 pickEl.innerText = response.pick.name;
                 ratingEl.innerText = response.pick.rating + ' ★';
                 let photoUrl = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=600&photoreference=' + response.pick.photos[0].photoReference + '&key=AIzaSyBL_9GfCUu7DGDvHdtlM8CaAywE2bVFVJc';
+                localStorage.setItem("restaurantAddress", response.pick.vicinity);
                 loadImage(photoUrl);
+                calculateAndDisplayRoute(directionsService, directionsRenderer);
             } else if (response.status === "INVALID_REQUEST") throw "Invalid request";
             else if (response.status === "ZERO_RESULTS") throw "No results";
             else if (response.status === "NO_REROLLS") throw "No re-rolls left";
@@ -242,6 +245,7 @@ function resultsPage(name, rating, photoUrl) {
             document.getElementById("pick").innerText = name;
             document.getElementById("rating").innerText = rating;
             loadImage(photoUrl);
+            addMapScript();
         });
 }
 
@@ -253,4 +257,50 @@ function loadImage(photoUrl) {
     let img = document.createElement('img');
     img.src = photoUrl;
     photoEl.appendChild(img);
+}
+
+/*=========================
+    Directions to the Restaurant
+ =========================*/
+ let directionsRenderer;
+ let directionsService;
+function addMapScript(){
+    let script = document.createElement('script');
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDbEPugXWcqo1q6b-X_pd09a0Zaj3trDOw&callback=initMap';
+    script.defer = true;
+    script.async = true;
+    document.head.appendChild(script);
+}
+
+function initMap() {
+  directionsRenderer = new google.maps.DirectionsRenderer();
+  directionsService = new google.maps.DirectionsService();
+  let lat = localStorage.getItem("lat")
+  let lng = localStorage.getItem("lng")
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 16,
+    center: { lat: parseFloat(lat), lng: parseFloat(lng)}
+  });
+  directionsRenderer.setMap(map);
+  directionsRenderer.setPanel(document.getElementById("directionsPanel"));
+  calculateAndDisplayRoute(directionsService, directionsRenderer);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  let start = localStorage.getItem("lat") + "," + localStorage.getItem("lng");
+  let end = localStorage.getItem("restaurantAddress");
+  directionsService.route(
+    {
+      origin: start,
+      destination: end,
+      travelMode: "DRIVING"
+    },
+    function(response, status) {
+      if (status === "OK") {
+        directionsRenderer.setDirections(response);
+      } else {
+        window.alert("Directions request failed due to " + status);
+      }
+    }
+  );
 }
