ee95b1acfba9d07366ba7103774b4d02577d1f05
ryan <ryangudal@gmail.com>
2020-07-22 15:52:39 +0000

    IOException caught

--- src/main/java/com/google/sps/data/AccessSecret.java
+++ src/main/java/com/google/sps/data/AccessSecret.java
@@ -4,6 +4,7 @@ import com.google.cloud.secretmanager.v1.AccessSecretVersionResponse;
 import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
 import com.google.cloud.secretmanager.v1.SecretVersionName;
 import java.io.IOException;
+import java.lang.Throwable;
 
 public class AccessSecret {
   private String key = null;
@@ -11,23 +12,25 @@ public class AccessSecret {
 
   private AccessSecret(){}
 
-  private String accessSecretVersion() throws IOException {
+  private String accessSecretVersion() {
     String projectId = "team-38-step-2020";
     String secretId = "API_KEY";
     String versionId = "2";
     return accessSecretVersion(projectId, secretId, versionId);
   }
 
-  private String accessSecretVersion(String projectId, String secretId, String versionId)
-      throws IOException {
+  private String accessSecretVersion(String projectId, String secretId, String versionId) {
     try (SecretManagerServiceClient client = SecretManagerServiceClient.create()) {
       SecretVersionName secretVersionName = SecretVersionName.of(projectId, secretId, versionId);
       AccessSecretVersionResponse response = client.accessSecretVersion(secretVersionName);
       return response.getPayload().getData().toStringUtf8();
+    } catch(IOException e){
+        e.printStackTrace(System.out);
+        return null;
     }
   }
 
-  public String getKey() throws IOException {
+  public String getKey() {
     if(this.key != null){
         return this.key;
     }
