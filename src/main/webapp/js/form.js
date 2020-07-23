49bef860e81417332991dc7e33e5bccd6a81be02
Jason Han <jjhan@vassar.edu>
2020-07-23 15:08:53 -0400

    use barba.js framework, ditch smoothState.js

--- src/main/webapp/js/form.js
+++ src/main/webapp/js/form.js
@@ -1,6 +1,31 @@
+$("#randomize-form").submit(function(event) {
+    const errorEl = document.getElementById("error");
+    errorEl.classList.add("hidden");
+
+    event.preventDefault();
+    let url = $(this).attr("action");
+    let lat = localStorage.getItem("lat");
+    let lng = localStorage.getItem("lng");
+    let userID = 0;
+    if (localStorage.getItem("loggedIn")) {
+        userID = localStorage.getItem("user");
+    }
+    let queryStr = $(this).serialize() + `&lat=${lat}&lng=${lng}&user=${userID}`;
+    query(queryStr);
+});
+
+// Form underline element
+$("input, textarea").blur(function() {
+    if ($(this).val() != "") {
+        $(this).addClass("active");
+    } else {
+        $(this).removeClass("active");
+    }
+});
+
 //This file handles the formatting for the form on RRP Homepage
 //This is done with a third-party formatting and styling package
-(function ($) {
+(function($) {
     'use strict';
     /*==================================================================
         [ Daterangepicker ]*/
@@ -17,45 +42,45 @@
         var myCalendar = $('.js-datepicker');
         var isClick = 0;
 
-        $(window).on('click',function(){
+        $(window).on('click', function() {
             isClick = 0;
         });
 
-        $(myCalendar).on('apply.daterangepicker',function(ev, picker){
+        $(myCalendar).on('apply.daterangepicker', function(ev, picker) {
             isClick = 0;
             $(this).val(picker.startDate.format('MM/DD/YYYY'));
 
         });
 
-        $('.js-btn-calendar').on('click',function(e){
+        $('.js-btn-calendar').on('click', function(e) {
             e.stopPropagation();
 
-            if(isClick === 1) isClick = 0;
-            else if(isClick === 0) isClick = 1;
+            if (isClick === 1) isClick = 0;
+            else if (isClick === 0) isClick = 1;
 
             if (isClick === 1) {
                 myCalendar.focus();
             }
         });
 
-        $(myCalendar).on('click',function(e){
+        $(myCalendar).on('click', function(e) {
             e.stopPropagation();
             isClick = 1;
         });
 
-        $('.daterangepicker').on('click',function(e){
+        $('.daterangepicker').on('click', function(e) {
             e.stopPropagation();
         });
 
 
-    } catch(er) {console.log(er);}
+    } catch (er) { console.log(er); }
     /*[ Select 2 Config ]
         ===========================================================*/
 
     try {
         var selectSimple = $('.js-select-simple');
 
-        selectSimple.each(function () {
+        selectSimple.each(function() {
             var that = $(this);
             var selectBox = that.find('select');
             var selectDropdown = that.find('.select-dropdown');
