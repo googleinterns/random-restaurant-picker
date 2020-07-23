ff7d05452fcca1ad84d0e8a31b66cd0a2b0bf0dd
ryan <ryangudal@gmail.com>
2020-07-23 01:37:32 +0000

    Add more tests for datastore and login

--- src/test/java/com/google/sps/SearchServletTest.java
+++ src/test/java/com/google/sps/SearchServletTest.java
@@ -33,9 +33,11 @@ import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+
 import org.mockito.Mock;
 import org.mockito.ArgumentCaptor;
-import static org.mockito.ArgumentMatchers.*;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
@@ -92,8 +94,8 @@ public final class SearchServletTest {
 
       DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
       List<Entity> results = ds.prepare(new Query("savedSearch")).asList(FetchOptions.Builder.withDefaults());
-      Assert.assertEquals(1, ds.prepare(new Query("savedSearch")).countEntities());
-      Assert.assertEquals((String)results.get(0).getProperty("keywords"), "coffee");
+      assertEquals(1, ds.prepare(new Query("savedSearch")).countEntities());
+      assertEquals((String)results.get(0).getProperty("keywords"), "coffee");
   }
 
   @Test
@@ -124,8 +126,8 @@ public final class SearchServletTest {
       //Process data and check correctness
       String results = sw.getBuffer().toString().trim();
       JsonObject resultsObj = (new JsonParser().parse(results)).getAsJsonArray().get(0).getAsJsonObject();
-      Assert.assertEquals(resultsObj.get("user").getAsString(), "2");
-      Assert.assertEquals(resultsObj.get("keywords").getAsString(), "indian");
+      assertEquals(resultsObj.get("user").getAsString(), "2");
+      assertEquals(resultsObj.get("keywords").getAsString(), "indian");
   }
 
   @Test
@@ -144,7 +146,7 @@ public final class SearchServletTest {
 
       //Process data and check correctness
       String results = sw.getBuffer().toString().trim();
-      Assert.assertEquals(results, "[]");
+      assertEquals(results, "[]");
   }
 
   @Test
@@ -175,6 +177,50 @@ public final class SearchServletTest {
 
       //Process data and check correctness
       String results = sw.getBuffer().toString().trim();
-      Assert.assertEquals(results, "[]");
+      assertEquals(results, "[]");
+  }
+
+  @Test
+  public void GETTwoInOrder() throws IOException{
+      //Test that the servlet returns the items in correct order
+      //when two matching entities are in the datastore
+      DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+      Entity searchEntity = new Entity("savedSearch");
+      searchEntity.setProperty("user", "2");
+      searchEntity.setProperty("radius", "1000");
+      searchEntity.setProperty("date", "Jul 17, at 13:21");
+      searchEntity.setProperty("keywords", "indian");
+      searchEntity.setProperty("timestamp", 112030394);
+      searchEntity.setProperty("lat", "40");
+      searchEntity.setProperty("lng", "-80");
+      ds.put(searchEntity);
+
+      Entity searchEntity2 = new Entity("savedSearch");
+      searchEntity2.setProperty("user", "2");
+      searchEntity2.setProperty("radius", "2500");
+      searchEntity2.setProperty("date", "Jul 17, at 13:28");
+      searchEntity2.setProperty("keywords", "mexican");
+      searchEntity2.setProperty("timestamp", 122030394);
+      searchEntity2.setProperty("lat", "40");
+      searchEntity2.setProperty("lng", "-80");
+      ds.put(searchEntity2);
+
+      //Handle calls to the mock objects
+      when(request.getParameter("user")).thenReturn("2");
+      StringWriter sw = new StringWriter();
+      PrintWriter pw = new PrintWriter(sw);
+      when(response.getWriter()).thenReturn(pw);
+
+      //Run the servlet and verify functions are called
+      new SearchServlet().doGet(request, response);
+      verify(response).setContentType("application/json;");
+      verify(response).getWriter();
+
+      //Process data and check correctness
+      String results = sw.getBuffer().toString().trim();
+      JsonObject resultsObj1 = (new JsonParser().parse(results)).getAsJsonArray().get(0).getAsJsonObject();
+      JsonObject resultsObj2 = (new JsonParser().parse(results)).getAsJsonArray().get(1).getAsJsonObject();
+      assertEquals(resultsObj1.get("keywords").getAsString(), "mexican");
+      assertEquals(resultsObj2.get("keywords").getAsString(), "indian");
   }
 }
