358e792e60b027036e504d3ef232fafb23a14878
Jason Han <jjhan@vassar.edu>
2020-07-26 11:37:42 -0400

    sidebar menu; better navigation

--- src/main/webapp/js/nav.js
+++ src/main/webapp/js/nav.js
@@ -4,79 +4,169 @@ function redirectToUrl(url) {
     barba.go(url)
 }
 
-const loadingScreen = document.querySelector('.loading-screen')
-const mainNavigation = document.querySelector('.main-navigation')
+function isTransitioning() { return document.body.classList.contains('is-transitioning') }
 
-function pageTransitionIn() {
-    return gsap
-        .to(loadingScreen, { duration: .5, scaleY: 1, transformOrigin: 'bottom left' })
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function pageTransitionOut(container) {
+function defaultTransition(currContainer, nextContainer) {
+    const transitionTitle = document.querySelector('.transition-title');
+    const transitionBackground = document.querySelector('.transition-background');
+    transitionTitle.innerHTML = capitalizeFirstLetter(nextContainer.dataset.barbaNamespace);
+    // transitionTitle.innerHTML = "You're headed to...";
     return gsap
-        .timeline({ delay: 0.25 })
-        .add('start')
-        .to(loadingScreen, {
-            duration: 0.5,
-            scaleY: 0,
-            skewX: 0,
-            transformOrigin: 'top left',
-            ease: 'power1.out'
-        }, 'start')
-        .call(contentAnimation, [container], 'start')
-}
-
-function contentAnimation(container) {
-    $(container.querySelector('.green-heading-bg')).addClass('show')
-    return gsap
-        .timeline()
-        .from(container.querySelector('.is-animated'), {
-            duration: 0.5,
-            translateY: 10,
-            opacity: 0,
-            stagger: 0.4
+        .timeline({
+            onComplete: () => {
+                transitionTitle.innerHTML = '';
+            }
         })
-        .from(mainNavigation, { duration: .5, translateY: -10, opacity: 0 })
+        .set(transitionBackground, {
+            clearProps: 'all'
+        })
+        .set(transitionTitle, {
+            y: 100
+        })
+        .to(transitionBackground, {
+            duration: 0.7,
+            x: '0',
+            ease: 'power4',
+            onComplete: () => {
+                currContainer.remove()
+            }
+        })
+        .to(transitionTitle, 0.5, {
+            y: 0,
+            opacity: 1,
+            ease: 'power4'
+        }, 0.1)
+        .from(nextContainer, {
+            duration: 0.1,
+            opacity: 0,
+            ease: 'power4'
+        })
+        .to(transitionBackground, {
+            duration: 0.7,
+            x: '100%',
+            ease: 'power4.inOut'
+        }, 1)
+        .to(transitionTitle, 0.7, {
+            y: -100,
+            opacity: 0,
+            ease: 'power4.inOut'
+        }, 0.8)
+        .then();
 }
+
+function toResults(currContainer, nextContainer) {
+    const transitionTitle = document.querySelector('.transition-title');
+    const transitionBackground = document.querySelector('.transition-background');
+    transitionTitle.innerHTML = "You're headed to...";
+    return gsap
+        .timeline({
+            onComplete: () => {
+                transitionTitle.innerHTML = '';
+            }
+        })
+        .set(transitionBackground, {
+            clearProps: 'all'
+        })
+        .set(transitionTitle, {
+            y: 100
+        })
+        .to(transitionBackground, {
+            duration: 0.7,
+            x: '0',
+            ease: 'power4',
+            onComplete: () => {
+                currContainer.remove()
+            }
+        })
+        .to(transitionTitle, 0.5, {
+            y: 0,
+            opacity: 1,
+            ease: 'power4'
+        }, 0.1)
+        .from(nextContainer, {
+            duration: 0.1,
+            opacity: 0,
+            ease: 'power4'
+        })
+        .to(transitionBackground, {
+            duration: 0.7,
+            x: '100%',
+            ease: 'power4.inOut'
+        }, 2)
+        .to(transitionTitle, 0.7, {
+            y: -100,
+            opacity: 0,
+            ease: 'power4.inOut'
+        }, 1.8)
+        .then();
+}
+
+
+// function contentAnimation(container) {
+//     $(container.querySelector('.green-heading-bg')).addClass('show')
+//     return gsap
+//         .timeline()
+//         .from(container.querySelector('.is-animated'), {
+//             duration: 0.5,
+//             translateY: 10,
+//             opacity: 0,
+//             stagger: 0.4
+//         })
+// }
 
 $(function() {
     barba.init({
         preventRunning: true,
-        transitions: [{
-                from: { namespace: ['search'] },
-                to: { namespace: ['results'] },
+        transitions: [
+            // Default
+            {
                 async leave(data) {
-                    await pageTransitionIn()
-                    data.current.container.remove()
+                    document.body.classList.add('is-transitioning');
+                    document.body.classList.add('prevent-scroll');
+                    if (isMenuOpen())
+                        menuClose();
                 },
 
                 async enter(data) {
-                    await pageTransitionOut(data.next.container)
-                    addMapScript()
-                    roll()
-                },
-
-                async once(data) { await contentAnimation(data.next.container); }
+                    await defaultTransition(data.current.container, data.next.container);
+                    document.body.classList.remove('is-transitioning');
+                    document.body.classList.remove('prevent-scroll');
+                }
             },
+            // To results
             {
-                from: { namespace: ['results'] },
-                to: { namespace: ['search'] },
+                to: { namespace: ['results'] },
                 async leave(data) {
-                    await pageTransitionIn()
-                    data.current.container.remove()
+                    document.body.classList.add('is-transitioning');
+                    document.body.classList.add('prevent-scroll');
+                    if (isMenuOpen())
+                        menuClose();
                 },
 
-                async enter(data) { await pageTransitionOut(data.next.container) },
-
-                async once(data) { await contentAnimation(data.next.container); },
-
-                async beforeEnter(data) {
-                    $.getScript("third-party/select2/select2.min.js");
-                    $.getScript("third-party/datepicker/moment.min.js");
-                    $.getScript("third-party/datepicker/daterangepicker.js");
-                    $.getScript("js/form.js");
+                async enter(data) {
+                    await toResults(data.current.container, data.next.container);
+                    document.body.classList.remove('is-transitioning');
+                    document.body.classList.remove('prevent-scroll');
                 }
             }
+            // {
+            //     from: { namespace: ['search'] },
+            //     to: { namespace: ['results'] },
+            //     async leave(data) {
+            //         await pageTransitionIn()
+            //         data.current.container.remove()
+            //     },
+
+            //     async enter(data) {
+            //         await pageTransitionOut(data.next.container)
+            //     },
+
+            //     async once(data) { await contentAnimation(data.next.container); }
+            // }
         ]
     });
 });
